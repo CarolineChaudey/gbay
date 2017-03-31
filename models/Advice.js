2d332d89d8129758ad9f76f9562754f21acb3c7d
Caroline Chaudey <caroline.chaudey@digiplug.local>
2017-03-31 12:44:20 +0200

    initialized model/index.js

---
+++ models/Advice.js
@@ -0,0 +1,11 @@
+const Schema = require('mongoose').Schema;
+const timestamps = require('mongoose-timestamps');
+
+module.exports = (api) => {
+    const schema = new Schema({
+
+    });
+
+    schema.plugin(timestamps);
+    return api.mongoose.model('Advice', schema);
+}
--- models/index.js
+++ models/index.js
@@ -1,7 +1,14 @@
-module.exports = (api) => {
-    //api.mongoose = mongoose.connect(api.settings.db.url);
-    //api.mongoose.promise = Promise;
-    api.models = {
-        
-    };
+const mongoose = require('mongoose');
+const bluebird = require('bluebird');
+
+module.exports = (server) => {
+    server.mongoose = mongoose.connect(server.settings.db.url);
+    server.mongoose.Promise = bluebird;
+    server.models = {
+        Advice: require('./Advice')(server),
+        Product: require('./Product')(server),
+        Role: require('./Role')(server),
+        Token: require('./Token')(server),
+        User: require('./User')(server),
+    }
 };
