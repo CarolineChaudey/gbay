a1c59460a43123b55a07240ed477f1c0a520a761
caroline <caroline.chaudey.sio@gmail.com>
2017-04-22 18:34:16 +0200

    ajout ok mais pas de statut

--- actions/auth/createUser.js
+++ actions/auth/createUser.js
@@ -5,6 +5,7 @@ module.exports = (api) => {
     const User = api.models.User;
 
     return function create(req, res, next) {
+
       // trouve ou créé l'objet
       User.findOrCreate({where: {email: req.body.email}, defaults: {pswd: sha1(req.body.password), address: req.body.address}})
       .spread(function(user, created) {
--- middlewares/ensureUserFields.js
+++ middlewares/ensureUserFields.js
@@ -1,5 +1,6 @@
 
 module.exports = (req , res, next) => {
+  console.log(req.body);
   if (!req.body || !req.body.email || !req.body.password || !req.body.address) {
     return res.status(400).send('missing.fields');
   }
