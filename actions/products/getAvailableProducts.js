7ccd4e0bc22714e74a274f7fe4288c1ca65d7e72
caroline <caroline.chaudey.sio@gmail.com>
2017-04-24 23:06:18 +0200

    pas encore testé

--- actions/products/getAvailableProducts.js
+++ actions/products/getAvailableProducts.js
@@ -2,29 +2,67 @@
 module.exports = (api) => {
   const Product = api.models.Product;
   const Category = api.models.Category;
+  const User = api.models.User;
 
   return function create(req, res, next) {
 
-    let productAvailableCondition = [
-      { // enchere non achevee
-        biddingEnd: {
-          $gt: new Date()
-        }
-      }, /* ici normalement l'opérateur OU agit */ {
-        // produit en vente mais pas encore achete
-        biddingEnd: null,
-        buyerUserId: null
-      }
-    ];
+    let querySettings = {};
 
-    Product.findAll({
-      where: {
-        $or: productAvailableCondition  // + propre de decomposer la structure
-      },
-      order: [
-        ['updatedAt', 'DESC']
+    querySettings.where = {
+      $or: [
+        { // enchere non achevee
+          biddingEnd: {
+            $gt: new Date()
+          }
+        }, /* ici normalement l'opérateur OU agit */ {
+          // produit en vente mais pas encore achete
+          biddingEnd: null,
+          buyerUserId: null
+        }
       ]
-    }).then(function(products) {
+    };
+
+    // les relations avec les autres tables
+    if (req.params.categories) {
+      querySettings.include.push({
+        model: Category,
+        where: {
+          title: {
+            $in: req.params.categories
+        }}
+      });
+    }
+    if (req.params.sellers) {
+      querySettings.include.push({
+        model: User,
+        where: {
+          userId: {
+            $in: req.params.sellers
+        }}
+      });
+    }
+
+    // pour l'ordre par prix (par date par défaut)
+    if (req.params.price) {
+      querySettings.order = [
+        ['price']
+      ];
+    } else {
+      querySettings.order = [
+        ['updatedAt', 'DESC']
+      ];
+    }
+
+    // pour la pagination
+    if (req.params.limit) {
+      querySettings.limit = req.params.limit;
+    }
+    if (req.params.offset) {
+      querySettings.offset = req.params.offset;
+    }
+
+    Product.findAll(querySettings)
+    .then(function(products) {
       if (!products || products.length == 0) {
         return res.status(204).send(products)
       }
