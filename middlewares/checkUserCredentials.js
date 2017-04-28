e0e5fe11abb604529dd4ab2fcbc32705928d62f3
caroline <caroline.chaudey.sio@gmail.com>
2017-04-28 15:08:53 +0200

    middleware ok

---
+++ middlewares/checkUserCredentials.js
@@ -0,0 +1,26 @@
+const sha1 = require('sha1');
+
+module.exports = (api) => {
+  const User = api.models.User;
+
+  return function checkUserCredentials(req, res, next) {
+
+    if (!req.body || !req.body.email || !req.body.password) {
+      return res.status(400).send('missing.fields');
+    }
+
+    let queryParams = {
+      where : {
+        email: req.body.email,
+        pswd: sha1(req.body.password)
+      }
+    };
+
+    User.findOne(queryParams).then(function(user) {
+      if (user) {
+        return res.status(200).send(user);
+      }
+      return res.status(400).send('incorrect.email.or.password');
+    });
+  };
+};
